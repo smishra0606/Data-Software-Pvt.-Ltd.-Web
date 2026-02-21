ab9e45df9d95433da1b5240b9bd581d264db5fd6
Shiven Mishra <144209847+smishra0606@users.noreply.github.com>
2026-02-22 02:31:35 +0530

    Import AuthSuccess component in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import AuthSuccess from "./pages/AuthSuccess";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { useEffect } from "react";
